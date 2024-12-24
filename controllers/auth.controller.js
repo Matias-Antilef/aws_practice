0bd0272c0bcfef812ad9e247cc6cbc35ce73099e
Matias Antilef <matias.romero.dv@gmail.com>
2024-12-24 15:59:46 -0300

    a

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,27 +2,20 @@ const db = require('../models');
 const User = db.User;
 
 const login = async (req, res) => {
-    const { email, password } = req.body; // Recibiendo email y password desde el body de la solicitud
+    const { email, password } = req.body;
     try {
-        // Buscar al usuario en la base de datos por su email
         const user = await User.findOne({ where: { email } });
 
         if (!user) {
-            // Si el usuario no existe
             return res.status(404).json({ message: 'Usuario no encontrado' });
         }
 
-        // Verificar si la contraseña es correcta
-        // Para este ejemplo, simplemente compararemos las contraseñas directamente (sin cifrado)
         if (user.password === password) {
-            // Si la contraseña es correcta
             return res.status(200).json({ message: 'Login exitoso', user });
         } else {
-            // Si la contraseña es incorrecta
             return res.status(401).json({ message: 'Contraseña incorrecta' });
         }
     } catch (error) {
-        // Manejo de errores
         console.error(error);
         return res.status(500).json({ message: 'Error al procesar la solicitud', error });
     }
